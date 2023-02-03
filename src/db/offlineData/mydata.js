7faed5a9a026d663ec676efab193091b04171323
nirmal <soulok9301@gmail.com>
2023-02-03 12:14:23 +0530

    category mapping changed

--- src/db/offlineData/mydata.js
+++ src/db/offlineData/mydata.js
@@ -1,24 +1,30 @@
 [
   {
-    head: "serverse,desktops,notebooks",
+    id: "1",
+    head: "Serverse,Desktops,Notebooks",
     categories: [
       {
+        id: "1/1",
         displayname: "Servers",
         category: "Servers",
       },
       {
+        id: "1/2",
         displayname: "All In One, Desktops & workstations",
         category: "All",
       },
       {
+        id: "1/3",
         displayname: "Notebooks/Laptops",
         category: "Notebooks",
       },
       {
+        id: "1/4",
         displayname: "Tablet PCs",
         category: "Tablet",
       },
       {
+        id: "1/5",
         displayname: "Notebook & Tablet Accessories",
         category: "Notebook",
       },
@@ -26,507 +32,256 @@
   },
 
   {
+    id: "2",
     head: "networking & communication",
     categories: [
+      { id: "2/1", displayname: "Pagers", category: "Pagers" },
+      { id: "2/2", displayname: "PCMCIA Cards", category: "PCMCIA" },
       {
-        displayname: "Pagers",
-        category: "Pagers",
-      },
-      {
-        displayname: "PCMCIA Cards",
-        category: "PCMCIA",
-      },
-      {
+        id: "2/3",
         displayname: "Connectors & Components",
         category: "Connectors & Components",
       },
+      { id: "2/4", displayname: "Print Servers", category: "Print Servers" },
+      { id: "2/1", displayname: "Internet Gateways", category: "" },
+      { id: "2/5", displayname: "Fax/Modems", category: "Fax Modems" },
+      { id: "2/6", displayname: "Tranceivers & modules", category: "" },
+      { id: "2/7", displayname: "VOIP Products ", category: "VOIP" },
+      { id: "2/8", displayname: "Wireless Products", category: "" },
+      { id: "2/9", displayname: "Networking Cable", category: "Networking" },
       {
-        displayname: "Print Servers",
-        category: "Print Servers",
-      },
-      {
-        displayname: "Internet Gateways",
-        category: "",
-      },
-      {
-        displayname: "Fax/Modems",
-        category: "Fax Modems",
-      },
-      {
-        displayname: "Tranceivers & modules",
-        category: "",
-      },
-      {
-        displayname: "VOIP Products ",
-        category: "VOIP",
-      },
-      {
-        displayname: "Wireless Products",
-        category: "",
-      },
-      {
-        displayname: "Networking Cable",
-        category: "Networking",
-      },
-      {
+        id: "2/10",
         displayname: "Redundant Power Supply",
         category: "Redundant",
       },
+      { id: "2/11", displayname: "2 Way Radios", category: "Redundant" },
+      { id: "2/12", displayname: "Switches & Hubs", category: "Redundant" },
+      { id: "2/13", displayname: "Gigabite", category: "Redundant" },
+      { id: "2/14", displayname: "ADSL/Cable modems", category: "Redundant" },
+      { id: "2/15", displayname: "Access Point", category: "Redundant" },
+      { id: "2/16", displayname: "Network Adapters", category: "Redundant" },
+      { id: "2/17", displayname: "Firewalls", category: "Redundant" },
       {
-        displayname: "2 Way Radios",
-        category: "Redundant",
-      },
-      {
-        displayname: "Switches & Hubs",
-        category: "Redundant",
-      },
-      {
-        displayname: "Gigabite",
-        category: "Redundant",
-      },
-      {
-        displayname: "ADSL/Cable modems",
-        category: "Redundant",
-      },
-      {
-        displayname: "Access Point",
-        category: "Redundant",
-      },
-      {
-        displayname: "Network Adapters",
-        category: "Redundant",
-      },
-      {
-        displayname: "Firewalls",
-        category: "Redundant",
-      },
-      {
+        id: "2/18",
         displayname: "Network Management Software",
         category: "Redundant",
       },
       {
+        id: "2/19",
         displayname: "Analog & Digital Telephones",
         category: "Redundant",
       },
       {
+        id: "2/20",
         displayname: "PABX & Telephone Accessories",
         category: "Redundant",
       },
-      {
-        displayname: "Cabinet & Racks",
-        category: "Redundant",
-      },
-      {
-        displayname: "Routers",
-        category: "Redundant",
-      },
+      { id: "2/21", displayname: "Cabinet & Racks", category: "Redundant" },
+      { id: "2/22", displayname: "Routers", category: "Redundant" },
     ],
   },
   {
+    id: "3",
     head: "storage components & accessories",
     categories: [
+      { id: "3/1", displayname: "Monitors", category: "Redundant" },
+      { id: "3/2", displayname: "Hard Drives", category: "Redundant" },
+      { id: "3/3", displayname: "Memory", category: "Redundant" },
       {
-        displayname: "Monitors",
-        category: "Redundant",
-      },
-      {
-        displayname: "Hard Drives",
-        category: "Redundant",
-      },
-      {
-        displayname: "Memory",
-        category: "Redundant",
-      },
-      {
+        id: "3/4",
         displayname: "Type/Data storage Drives",
         category: "Redundant",
       },
+      { id: "3/5", displayname: "Duplicators", category: "Redundant" },
+      { id: "3/6", displayname: "CPU", category: "Redundant" },
+      { id: "3/7", displayname: "Motherboards", category: "Redundant" },
+      { id: "3/8", displayname: "Controller Cards", category: "Redundant" },
+      { id: "3/9", displayname: "Sound Cards", category: "Redundant" },
+      { id: "3/10", displayname: "VGA Cards", category: "Redundant" },
+      { id: "3/11", displayname: "Power Supply Units", category: "Redundant" },
+      { id: "3/12", displayname: "External HDD", category: "Redundant" },
+      { id: "3/13", displayname: "DVD Drives", category: "Redundant" },
+      { id: "3/14", displayname: "DVD Writers", category: "Redundant" },
+      { id: "3/15", displayname: "Floppy Drives", category: "Redundant" },
       {
-        displayname: "Duplicators",
-        category: "Redundant",
-      },
-      {
-        displayname: "CPU",
-        category: "Redundant",
-      },
-      {
-        displayname: "Motherboards",
-        category: "Redundant",
-      },
-      {
-        displayname: "Controller Cards",
-        category: "Redundant",
-      },
-      {
-        displayname: "Sound Cards",
-        category: "Redundant",
-      },
-      {
-        displayname: "VGA Cards",
-        category: "Redundant",
-      },
-      {
-        displayname: "Power Supply Units",
-        category: "Redundant",
-      },
-      {
-        displayname: "External HDD",
-        category: "Redundant",
-      },
-      {
-        displayname: "DVD Drives",
-        category: "Redundant",
-      },
-      {
-        displayname: "DVD Writers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Floppy Drives",
-        category: "Redundant",
-      },
-      {
+        id: "3/16",
         displayname: "NAS Network Attached Storage",
         category: "Redundant",
       },
+      { id: "3/17", displayname: "SCSI cards", category: "Redundant" },
+      { id: "3/18", displayname: "USB Drives", category: "Redundant" },
+      { id: "3/19", displayname: "PCI cards", category: "Redundant" },
       {
-        displayname: "SCSI cards",
-        category: "Redundant",
-      },
-      {
-        displayname: "USB Drives",
-        category: "Redundant",
-      },
-      {
-        displayname: "PCI cards",
-        category: "Redundant",
-      },
-      {
+        id: "3/20",
         displayname: "Memory Cards & Readers",
         category: "Redundant",
       },
+      { id: "3/21", displayname: "Keyboards", category: "Redundant" },
       {
-        displayname: "Keyboards",
-        category: "Redundant",
-      },
-      {
+        id: "3/22",
         displayname: "Mice/Pointing Device",
         category: "Redundant",
       },
-      {
-        displayname: "Joystick",
-        category: "Redundant",
-      },
-      {
-        displayname: "Combo Drives",
-        category: "Redundant",
-      },
-      {
-        displayname: "Headset/Microphones",
-        category: "Redundant",
-      },
-      {
-        displayname: "TV Tuner Cards",
-        category: "Redundant",
-      },
+      { id: "3/23", displayname: "Joystick", category: "Redundant" },
+      { id: "3/24", displayname: "Combo Drives", category: "Redundant" },
+      { id: "3/25", displayname: "Headset/Microphones", category: "Redundant" },
+      { id: "3/26", displayname: "TV Tuner Cards", category: "Redundant" },
     ],
   },
 
   {
+    id: "4",
     head: "consumer electronics",
     categories: [
       {
+        id: "4/1",
         displayname: "Electronic Components & Accessories",
         category: "Redundant",
       },
       {
+        id: "4/2",
         displayname: "Digital/Normal Cameras",
         category: "Redundant",
       },
+      { id: "4/3", displayname: "Projectors", category: "Redundant" },
       {
-        displayname: "Projectors",
-        category: "Redundant",
-      },
-      {
+        id: "4/4",
         displayname: "Digital Video Products",
         category: "Redundant",
       },
+      { id: "4/5", displayname: "Mobile phones", category: "Redundant" },
       {
-        displayname: "Mobile phones",
-        category: "Redundant",
-      },
-      {
+        id: "4/6",
         displayname: "Global Position Systems",
         category: "Redundant",
       },
+      { id: "4/7", displayname: "Video Game Console", category: "Redundant" },
+      { id: "4/8", displayname: "Video Cameras", category: "Redundant" },
       {
-        displayname: "Video Game Console",
-        category: "Redundant",
-      },
-      {
-        displayname: "Video Cameras",
-        category: "Redundant",
-      },
-      {
+        id: "4/9",
         displayname: "Entertainment & Gaming",
         category: "Redundant",
       },
+      { id: "4/10", displayname: "Plasma", category: "Redundant" },
       {
-        displayname: "Plasma",
-        category: "Redundant",
-      },
-      {
+        id: "4/11",
         displayname: "Projector screens & Accessories",
         category: "Redundant",
       },
+      { id: "4/12", displayname: "Home Theatre System", category: "Redundant" },
+      { id: "4/13", displayname: "DVD Players", category: "Redundant" },
+      { id: "4/14", displayname: "Blueray", category: "Redundant" },
+      { id: "4/15", displayname: "LED/LCD TVs", category: "Redundant" },
       {
-        displayname: "Home Theatre System",
-        category: "Redundant",
-      },
-      {
-        displayname: "DVD Players",
-        category: "Redundant",
-      },
-      {
-        displayname: "Blueray",
-        category: "Redundant",
-      },
-      {
-        displayname: "LED/LCD TVs",
-        category: "Redundant",
-      },
-      {
+        id: "4/16",
         displayname: "MP3/MP4/Ipod Players & Accessories",
         category: "Redundant",
       },
-      {
-        displayname: "Security Cameras",
-        category: "Redundant",
-      },
-      {
-        displayname: "Speakers",
-        category: "Redundant",
-      },
+      { id: "4/17", displayname: "Security Cameras", category: "Redundant" },
+      { id: "4/18", displayname: "Speakers", category: "Redundant" },
     ],
   },
 
   {
+    id: "5",
     head: "printers & scanners",
     categories: [
       {
+        id: "5/1",
         displayname: "Scanner & Fax Copier Printer Parts",
         category: "Redundant",
       },
-      {
-        displayname: "Scanners",
-        category: "Redundant",
-      },
-      {
-        displayname: "Receipt Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Mobile Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Deskjet Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Photosmart Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Thermal Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "NIP Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Thermal Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Dotmatrix Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Plotters",
-        category: "Redundant",
-      },
-      {
-        displayname: "All in one Printers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Laser Printers",
-        category: "Redundant",
-      },
+      { id: "5/2", displayname: "Scanners", category: "Redundant" },
+      { id: "5/3", displayname: "Receipt Printers", category: "Redundant" },
+      { id: "5/4", displayname: "Mobile Printers", category: "Redundant" },
+      { id: "5/5", displayname: "Deskjet Printers", category: "Redundant" },
+      { id: "5/6", displayname: "Photosmart Printers", category: "Redundant" },
+      { id: "5/7", displayname: "Thermal Printers", category: "Redundant" },
+      { id: "5/8", displayname: "NIP Printers", category: "Redundant" },
+      { id: "5/9", displayname: "Thermal Printers", category: "Redundant" },
+      { id: "5/10", displayname: "Dotmatrix Printers", category: "Redundant" },
+      { id: "5/11", displayname: "Plotters", category: "Redundant" },
+      { id: "5/12", displayname: "All in one Printers", category: "Redundant" },
+      { id: "5/13", displayname: "Laser Printers", category: "Redundant" },
     ],
   },
 
   {
+    id: "6",
     head: "accessories & cables",
     categories: [
+      { id: "6/1", displayname: "Mount Kit", category: "Redundant" },
+      { id: "6/2", displayname: "Power Cords", category: "Redundant" },
+      { id: "6/3", displayname: "Cable Organisers", category: "Redundant" },
+      { id: "6/4", displayname: "VGA Cables", category: "Redundant" },
+      { id: "6/5", displayname: "Cable Organisers", category: "Redundant" },
+      { id: "6/6", displayname: "CD/Disk Storage", category: "Redundant" },
+      { id: "6/7", displayname: "Laplink Cables", category: "Redundant" },
+      { id: "6/8", displayname: "Modem Cables", category: "Redundant" },
+      { id: "6/9", displayname: "Fiber Optic Cables", category: "Redundant" },
+      { id: "6/10", displayname: "Networking Cables", category: "Redundant" },
+      { id: "6/11", displayname: "Tool Kit", category: "Redundant" },
+      { id: "6/12", displayname: "Data/KVM Switches", category: "Redundant" },
+      { id: "6/13", displayname: "USB Cables", category: "Redundant" },
+      { id: "6/14", displayname: "Firewire Cables", category: "Redundant" },
+      { id: "6/15", displayname: "Serial Cables", category: "Redundant" },
+      { id: "6/16", displayname: "Audio Video Cables", category: "Redundant" },
+      { id: "6/17", displayname: "Power Cables", category: "Redundant" },
       {
-        displayname: "Mount Kit",
-        category: "Redundant",
-      },
-      {
-        displayname: "Power Cords",
-        category: "Redundant",
-      },
-      {
-        displayname: "Cable Organisers",
-        category: "Redundant",
-      },
-      {
-        displayname: "VGA Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Cable Organisers",
-        category: "Redundant",
-      },
-      {
-        displayname: "CD/Disk Storage",
-        category: "Redundant",
-      },
-      {
-        displayname: "Laplink Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Modem Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Fiber Optic Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Networking Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Tool Kit",
-        category: "Redundant",
-      },
-      {
-        displayname: "Data/KVM Switches",
-        category: "Redundant",
-      },
-      {
-        displayname: "USB Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Firewire Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Serial Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Audio Video Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Power Cables",
-        category: "Redundant",
-      },
-      {
+        id: "6/18",
         displayname: "Keyboard Connectivity",
         category: "Redundant",
       },
-      {
-        displayname: "Dust Covers",
-        category: "Redundant",
-      },
-      {
-        displayname: "Monitor/UPS Cables",
-        category: "Redundant",
-      },
-      {
-        displayname: "Printer Cables",
-        category: "Redundant",
-      },
+      { id: "6/19", displayname: "Dust Covers", category: "Redundant" },
+      { id: "6/20", displayname: "Monitor/UPS Cables", category: "Redundant" },
+      { id: "6/21", displayname: "Printer Cables", category: "Redundant" },
     ],
   },
 
   {
+    id: "7",
     head: "consumables & supplies",
     categories: [
-      {
-        displayname: "Paper",
-        category: "Redundant",
-      },
-      {
-        displayname: "Data Storage/Disk",
-        category: "Redundant",
-      },
-      {
-        displayname: "Ribbons",
-        category: "Redundant",
-      },
-      {
-        displayname: "Toner Catridges",
-        category: "Redundant",
-      },
-      {
-        displayname: "Data/Tape Catridges",
-        category: "Redundant",
-      },
-      {
-        displayname: "Ink Catridges",
-        category: "Redundant",
-      },
+      { id: "7/1", displayname: "Paper", category: "Redundant" },
+      { id: "7/2", displayname: "Data Storage/Disk", category: "Redundant" },
+      { id: "7/3", displayname: "Ribbons", category: "Redundant" },
+      { id: "7/4", displayname: "Toner Catridges", category: "Redundant" },
+      { id: "7/5", displayname: "Data/Tape Catridges", category: "Redundant" },
+      { id: "7/6", displayname: "Ink Catridges", category: "Redundant" },
     ],
   },
 
   {
+    id: "8",
     head: "office products & equipment",
     categories: [
+      { id: "8/1", displayname: "Binding Products", category: "Redundant" },
+      { id: "8/2", displayname: "SAFES", category: "Redundant" },
+      { id: "8/3", displayname: "Cash Drawers", category: "Redundant" },
       {
-        displayname: "Binding Products",
-        category: "Redundant",
-      },
-      {
-        displayname: "SAFES",
-        category: "Redundant",
-      },
-      {
-        displayname: "Cash Drawers",
-        category: "Redundant",
-      },
-      {
+        id: "8/4",
         displayname: "Barcode Label Printers",
         category: "Redundant",
       },
-      {
-        displayname: "Scanner & Fax Copier",
-        category: "Redundant",
-      },
-      {
-        displayname: "Shredders",
-        category: "Redundant",
-      },
-      {
-        displayname: "POS Keyboards",
-        category: "Redundant",
-      },
-      {
-        displayname: "Laminators",
-        category: "Redundant",
-      },
-      {
-        displayname: "POS Accessories",
-        category: "Redundant",
-      },
+      { id: "8/5", displayname: "Scanner & Fax Copier", category: "Redundant" },
+      { id: "8/6", displayname: "Shredders", category: "Redundant" },
+      { id: "8/7", displayname: "POS Keyboards", category: "Redundant" },
+      { id: "8/8", displayname: "Laminators", category: "Redundant" },
+      { id: "8/9", displayname: "POS Accessories", category: "Redundant" },
     ],
   },
+  {
+    id: "9",
+    head: "Software",
+    categories:[
+      
+    ]
+  },
+  {
+    id: "10",
+    head: "Power Products & UPS",
+    categories:[
+      {}
+    ]
+  }
 ];
